80e006e9f206c84e80878ef85696a364759e3243
Nobuhito ARAI <nobht_arai@icloud.com>
2021-09-23 23:09:02 +0900

    add: script file.

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express()
+
+app.use('/graphql',graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+const port = 4000
+
+app.listen(port)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
